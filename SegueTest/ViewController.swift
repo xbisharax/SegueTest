a32e873d0cb55f307070ff19077b5ddad2e1a2c8
Bishara Said <bishara_said1@mymail.eku.edu>
2024-09-19 15:35:35 -0400

    Initial Commit

---
+++ SegueTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegueTest
+//
+//  Created by Geeks on 9/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
